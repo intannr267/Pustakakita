453f85e317d688e359764bc61064b36d193e6086
intan <intanrahmania267@gmail.com>
2025-08-01 04:53:09 +0700

    Invoice Qr

--- helpers/helper.js
+++ helpers/helper.js
@@ -1,14 +1,7 @@
-const borrowBook=()=>{
-  const today = new Date()
-  return today.toISOString().split('T')[0]; 
-}
-
-const returnDate = () => {
-  const today = new Date();
-  today.setDate(today.getDate() + 7);
-  return today.toISOString().split('T')[0]; 
+const formatDate = (date) => {
+  return date.toDateString();
 };
 
 module.exports = {
-  borrowBook,returnDate
+  formatDate
 };
